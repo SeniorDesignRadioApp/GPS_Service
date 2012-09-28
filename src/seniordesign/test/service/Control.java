9393e2ca69e66c040f80f79d40992f6f035648e7
Grayson Malinowski <mali0152@umn.edu>
2012-09-27 21:42:01 -0500

    Updated and tested on phone
    Updated so that the gps service will update constantly (can change
    though constants) and the display will update every 30 seconds (can
    change though constants) and the back button will ask the user if they
    want to quit or not and end the service. Also added a lot of comments

--- src/seniordesign/test/service/Control.java
+++ src/seniordesign/test/service/Control.java
@@ -1,18 +1,30 @@
 package seniordesign.test.service;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.View;
+import android.widget.TextView;
 
 public class Control extends Activity {
-	Intent in;
+	private Intent in;
+	public static TextView textView1;
 
+	/**
+	 * On startup starts the Intent "ShowMessages" and sets the textView1 to "On Create"
+	 */
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_control);
         in = new Intent(this, ShowMessages.class);
+
+        textView1 = (TextView) findViewById(R.id.textView1);
+        
+        changeText("On Create");
     }
 
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -20,13 +32,53 @@ public class Control extends Activity {
         return true;
     }
     
+    /**
+     * Changes textView1 to string msg
+     */
+    public static void changeText(String msg) {
+    	textView1.setText(msg);
+    }
+    
+    /**
+     * tell the service to start displaying GPS updates
+     */
     public void startMessages(View view) {
-    	/* tell the service to start displaying GPS updates */
     	startService(in);
     }
     
+    /**
+     * tell the service to stop displaying GPS updates
+     */
     public void stopMessages(View view) {
-    	/* tell the service to stop displaying GPS updates */
+    	changeText("stopped");
     	stopService(in);
+    }
+    
+    /**
+     * When the BACK key is pressed ask the user if they want to quit
+     * if they do then stop the service and exit the program
+     */
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event)
+    {
+        if ((keyCode == KeyEvent.KEYCODE_BACK)) {
+        	@SuppressWarnings("unused")
+			AlertDialog alertbox = new AlertDialog.Builder(this)
+            .setMessage("Do you want to exit the application?")
+            .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                // stop the service and end the program
+                public void onClick(DialogInterface arg0, int arg1) {
+                	stopService(in);
+                    finish();
+                }
+            })
+            .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                // return to program
+                public void onClick(DialogInterface arg0, int arg1) {}
+            })
+            
+            .show();
+        }
+        return super.onKeyDown(keyCode, event);
     }
 }
--- src/seniordesign/test/service/ShowMessages.java
+++ src/seniordesign/test/service/ShowMessages.java
@@ -7,21 +7,94 @@ import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
+import android.os.Handler;
 import android.os.IBinder;
 import android.widget.Toast;
-
+/**
+ * This service will start up and stop a thread to continuously find the current GPS location.
+ */
 public class ShowMessages extends Service implements LocationListener {
-	LocationManager lm;
+	//constants for requestLocationUpdates
+	private static final int MIN_TIME_MILLISECONDS = 0;
+	private static final int MIN_DIST_METERS = 0;
+	//constants for Thread (in milliseconds)
+	private final int THREAD_TIMER = 30*1000;
+	
+	private LocationManager lm;
+	private int display_count = 0;
+	//variables for thread
+	private boolean thread_started = false;
+	private Thread mThread = new Thread(new RepeatingThread());
+	private final Handler mHandler = new Handler();
+	//variables for averaging gps location
+	private double latitude_temp = 0;
+	private double longitude_temp = 0;
+	private int lat_long_count_temp = 0;
 
+	/**
+	 * On "startMessages" start LocationManager (gps service)
+	 *   setup requestLocationUpdates (by time and distance)
+	 *   change textView1 to "Service Started"
+	 *   setup thread
+	 */
 	public void onCreate() {
 		lm = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
-		lm.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this);
-		Toast.makeText(getApplicationContext(), "Location display is on", Toast.LENGTH_SHORT).show();
+		lm.requestLocationUpdates(LocationManager.GPS_PROVIDER, MIN_TIME_MILLISECONDS, 
+				MIN_DIST_METERS, this);
+		Toast.makeText(getApplicationContext(), "Location display is on", 
+				Toast.LENGTH_SHORT).show();
+		Control.changeText("Service Started");
+		
+		//only start thread if one isn't already running
+		if(!thread_started){
+			thread_started = true;
+			mThread.start();
+		}
 	}
 	
+	/**
+	 * Thread that is repeated every 30 seconds till "onDestroy" is called
+	 */
+	public class RepeatingThread implements Runnable {
+	    public RepeatingThread() {}
+	    /*
+	     * If the longitude, latitude or counter are still at 0 do nothing
+	     * Else find the average and display to screen
+	     */
+	    public void run() {
+	    	if(longitude_temp != 0 && latitude_temp != 0 && lat_long_count_temp != 0) {
+		    	double average_longitude = longitude_temp / lat_long_count_temp;
+		    	double averate_latitude = latitude_temp / lat_long_count_temp;
+		    	Control.changeText("Timer Started " + display_count + "\n" + 
+		    			"Latitude = " + averate_latitude + "\n" + 
+		    			"Longitude = " + average_longitude);
+		    	display_count++;
+		    	longitude_temp = 0;
+		    	latitude_temp = 0;
+		    	lat_long_count_temp = 0;
+	    	}
+	        mHandler.postDelayed(mThread, THREAD_TIMER);       
+	    }
+	}
+
+	/**
+	 * Called when "stopMessage" is run
+	 * Stops Threads and resets all variables
+	 */
 	public void onDestroy() {
 		lm.removeUpdates(this);
-		Toast.makeText(getApplicationContext(), "Location display is off", Toast.LENGTH_SHORT).show();
+		Toast.makeText(getApplicationContext(), "Location display is off", 
+				Toast.LENGTH_SHORT).show();
+		longitude_temp = 0;
+    	latitude_temp = 0;
+    	lat_long_count_temp = 0;
+    	//only stop thread if it's already running
+		if(thread_started){
+			mHandler.removeCallbacks(mThread);
+			if(mThread != null){mThread.interrupt();}
+			thread_started = false;
+			display_count = 0;
+		}
 	}
 
 	public IBinder onBind(Intent intent) {
@@ -31,9 +104,21 @@ public class ShowMessages extends Service implements LocationListener {
 	public void onStart() {
 	}
 
+	/**
+	 * Called when location is changed
+	 * If the thread is going then update location
+	 * Else reset location
+	 */
 	public void onLocationChanged(Location loc) {
-		String msg = "Location changed:  lat=" + loc.getLatitude() + "  long=" + loc.getLongitude();
-		Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_SHORT).show();
+		if(thread_started){
+			latitude_temp += loc.getLatitude();
+			longitude_temp += loc.getLongitude();
+			lat_long_count_temp++;
+		} else {
+			latitude_temp = 0;
+			longitude_temp = 0;
+			lat_long_count_temp = 0;
+		}
 	}
 
 	public void onProviderDisabled(String provider) {
